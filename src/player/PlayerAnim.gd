05058f3cb9f5473cc7eaf195cd96d01bd2f55a07
myin <minyin142p@gmail.com>
2023-04-23 16:19:38 +0200

    add jump logic

--- src/player/PlayerAnim.gd
+++ src/player/PlayerAnim.gd
@@ -2,5 +2,5 @@
 extends SpriteAnimTool
 
 func update():
-	add_animation("idle", 0, 1, 0.5)
+	add_animation("idle", 0, 1, 0.7)
 	add_animation("run", 2, 7, 1)
--- src/player/player.gd
+++ src/player/player.gd
@@ -7,21 +7,25 @@ extends CharacterBody2D
 
 @export var accel = 1000
 @export var speed = 200
+@export var jump_force = 300
+
+@onready var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 func _physics_process(delta):
 	var motion = input.get_action_strength("move_left") - input.get_action_strength("move_right")
 	
 	velocity.x = move_toward(velocity.x, motion * speed, accel * delta)
+	velocity += Vector2.DOWN * gravity * delta
 	
 	if motion != 0:
-		if motion < 0:
-			sprite.scale.x = -1
-		else:
-			sprite.scale.x = 1
+		sprite.scale.x = sign(motion)
 	
-	if abs(velocity.x) > 0:
-		anim.play("run")
-	else:
-		anim.play("idle")
+	anim.play("idle" if velocity.x == 0 else "run")
+	
 	
 	move_and_slide()
+
+
+func _on_player_input_just_pressed(ev: InputEvent):
+	if ev.is_action_pressed("jump") and is_on_floor():
+		velocity += Vector2.UP * jump_force
