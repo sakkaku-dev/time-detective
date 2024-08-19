2277f5f8c09043cdbd359d4fc77c0c9d85aa3045
myin <minyin142p@gmail.com>
2024-08-19 18:22:24 +0200

    remove unneeded files

--- addons/debug/Build.gd
+++
@@ -1,6 +0,0 @@
-class_name Build
-
-#####
-# NOTE: This file is replaced by the build system. DO NOT EDIT!
-#####
-const VERSION = ""
--- theme/generate-theme.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-sass theme/theme.scss theme/theme.css
-godot -s addons/godot-css-theme/convert.gd --input="res://theme/theme.css"
